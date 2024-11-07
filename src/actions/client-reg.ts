e90e71a061828d2a2c4f252f20948e66d14b642b
tanzid64 <tanzid3@gmail.com>
2024-11-07 21:19:19 +0600

    feat: get all services & get skills action created

---
+++ src/actions/client-reg.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import db from "@/lib/prisma";
+
+export async function getServices() {
+  // Get all services list
+  try {
+    const services = await db.service.findMany({
+      orderBy: {
+        title: "asc",
+      },
+    });
+    if (services.length < 1) {
+      return {
+        status: 404,
+        message: "No services found",
+      };
+    }
+    return {
+      status: 200,
+      services,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      status: 500,
+      message: "Internal server error",
+    };
+  }
+}
+
+export async function getSkillsUnderService(serviceId: string) {
+  // Get all skills under a service
+  try {
+    const skills = await db.skill.findMany({
+      where: {
+        serviceId,
+      },
+      orderBy: {
+        title: "asc",
+      },
+    });
+    if (skills.length < 1) {
+      return {
+        status: 404,
+        message: "No skills found",
+      };
+    }
+    return {
+      status: 200,
+      skills,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      status: 500,
+      message: "Internal server error",
+    };
+  }
+}
